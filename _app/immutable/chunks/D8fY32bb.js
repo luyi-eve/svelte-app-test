b030ef0c5cedab361f2812a9f3542e26792c4b12
luyi2022 <eseve@live.com>
2025-03-06 13:50:36 -0500

    Updates

--- _app/immutable/chunks/D8fY32bb.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_sg4l6j)==null?void 0:s.base)??"/svelte-app-test";var e;const a=((e=globalThis.__sveltekit_sg4l6j)==null?void 0:e.assets)??t;export{a,t as b};
